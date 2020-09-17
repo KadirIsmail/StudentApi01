74fc69dba971381620dc14e3038b7901bcda7a95
MONSTER <MONSTER@DESKTOP-CFTMKCQ>
2020-09-17 10:05:48 +0300

    ilk upload

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentApi01.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StudentApi01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly StudentDbContext _db;
+
+        public StudentController(StudentDbContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpGet]
+        public List<Student> GetStudents()//select
+        {
+            var StudList = _db.Students.AsNoTracking().ToList();
+            return StudList;
+        }
+
+        //Get Students by Id
+        [HttpGet("{id}")]
+        public Student GetEmployeeById(int id)
+        {
+            Student Stud = _db.Students.FirstOrDefault(s => s.Id == id);
+            return Stud;
+        }
+
+        [HttpPost]
+        public string Create(Student objStudent)
+        {
+            _db.Students.Add(objStudent);
+            _db.SaveChanges();
+            return "Save Successfully";
+        }
+
+        //Update Students Info
+        [HttpPut("{id}")]
+        public string UpdateStudent(Student objStudent)
+        {
+            _db.Students.Update(objStudent);
+            _db.SaveChanges();
+            return "Update Successfully";
+        }
+
+
+        //Delete students
+        [HttpDelete("{id}")]
+        public string DeleteEmployee(Student objStudent)
+        {
+            _db.Remove(objStudent);
+            _db.SaveChanges();
+            return "Delete Successfully";
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentApi01.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Class { get; set; }
+        public int Number { get; set; }
+    }
+}
